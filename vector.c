08a41cb2361064c11c90a537b25aad9f6349a61c
topologicalhurt <85112999+topologicalhurt@users.noreply.github.com>
2021-10-26 16:13:35 +1100

    Fixed memory issue with copying vectors, added ability to add and find product of vectors.

--- vector.c
+++ vector.c
@@ -14,7 +14,7 @@ vec* init_vec(size_t size) {
 
 vec* copy(vec* v) {
     vec* nvec = (vec*) malloc(VECTOR_SIZE);
-    memcpy(nvec, v, v -> alloc);
+    memcpy(nvec, v, VECTOR_SIZE);
     return nvec;
 }
 
@@ -55,16 +55,66 @@ void append(vec* v, float item) {
 void to_string(vec* v) {
     float* elems = v -> elems;
     size_t elem_size = v -> size;
-    char buffer[26]; // 24 char bytes for float, 1 char byte for comma, 1 char byte for null terminator
+    char buffer[0x1A]; // 24 char bytes for float, 1 char byte for comma, 1 char byte for null terminator
     printf("[");
     for(int i = 0; i < elem_size - 1; i++) {
-        snprintf(buffer, 26, "%f,", elems[i]); // load buffer
+        snprintf(buffer, 0x1A, "%f,", elems[i]); // load buffer
         printf("%s", buffer); // print chars from buffer
     };
     printf("%f]\n", elems[elem_size - 1]);
 }
 
-vec* scaled(vec* v, float s) {
+vec* sum(size_t n, vec* v, vec* v2, ...) {
+    if(n < 2) {
+        return NULL;
+    }
+    size_t elem_size = v -> size; // length of first vector is assumed dimension
+    vec* sumvec = copy(v);
+    for(int i = 0; i < elem_size; i++) {
+        sumvec -> elems[i] += v2 -> elems[i];
+    }
+    if(n == 2) {
+        return sumvec;
+    } else {
+        va_list args;
+        va_start(args, n - 2);
+        for(int i = 0; i < n - 2; i++) {
+            vec* arg = va_arg(args, vec*);
+            for(int j = 0; j < elem_size; j++) {
+                sumvec -> elems[j] += arg -> elems[j];
+            }
+        }
+        va_end(args);
+        return sumvec;
+    }
+}
+
+vec* prod(size_t n, vec* v, vec* v2, ...) {
+    if(n < 2) {
+        return NULL;
+    }
+    size_t elem_size = v -> size; // length of first vector is assumed dimension
+    vec* prodvec = copy(v);
+    for(int i = 0; i < elem_size; i++) {
+        prodvec -> elems[i] *= v2 -> elems[i];
+    }
+    if(n == 2) {
+        return prodvec;
+    } else {
+        va_list args;
+        va_start(args, n - 2);
+        for(int i = 0; i < n - 2; i++) {
+            vec* arg = va_arg(args, vec*);
+            for(int j = 0; j < elem_size; j++) {
+                prodvec -> elems[j] *= arg -> elems[j];
+            }
+        }
+        va_end(args);
+        return prodvec;
+    }
+}
+
+vec* scale(vec* v, float s) {
     vec* nvec = copy(v);
     for(int i = 0; i < v -> size; i++) {
         nvec -> elems[i] *= s;
@@ -95,7 +145,7 @@ float* dot(size_t n, vec* v, vec* v2, ...) {
         return pdot;
     } else {
         va_list args;
-        va_start(args, n);
+        va_start(args, n - 2);
         float product;
         for(int i = 0; i < elem_size; i++) {
             product = v -> elems[i] * v2 -> elems[i];
@@ -103,7 +153,7 @@ float* dot(size_t n, vec* v, vec* v2, ...) {
                 product *=  va_arg(args, vec*) -> elems[i];
             };
             dot_product += product;
-            va_start(args, n);
+            va_start(args, n - 2);
         };
         va_end(args);
         float* pdot = (float*)&dot_product;
@@ -158,7 +208,7 @@ static float fast_inverse_square_root(float n) {
 }
 
 vec* norm(vec* v) {
-    return scaled(v, fast_inverse_square_root(squared_mag(v)));
+    return scale(v, fast_inverse_square_root(squared_mag(v)));
 }
 
 vec* proj(vec* v, vec* v2) {
@@ -166,7 +216,7 @@ vec* proj(vec* v, vec* v2) {
     Return the vector corresponding to the projection of vector v onto vector v2
     */
     vec* nvec = copy(v2);
-    return scaled(nvec, *dot(2, v, v2) / squared_mag(v2));
+    return scale(nvec, *dot(2, v, v2) / squared_mag(v2));
 }
 
 vec* cross(vec* v, vec* v2) {
