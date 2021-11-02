c89f8cd04bdc898cb37659e12b0bb9c7af9c31c3
topologicalhurt <85112999+topologicalhurt@users.noreply.github.com>
2021-11-02 16:48:07 +1100

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -11,10 +11,15 @@ typedef struct mat {
 
 mat* init_mat(size_t w, size_t h);
 void to_stringm(mat* m);
+void* filterm(mat* m, vec* target);
+mat* splicem(const mat* m, size_t w1, size_t w2);
 void delm(mat* m);
+size_t countm(const mat* m, const vec* v);
 void* setm(mat* m, size_t x, size_t y, float replacement);
 void* resizem(mat* m, size_t w, size_t h);
 mat* identity(size_t w, size_t h);
-float* det(mat* m);
+float* detm(mat* m);
+int cmpmx(const void* p1, const void* p2);
+int cmpmy(const void* p1, const void* p2);
 
 #endif // MATRIX_H_INCLUDED
